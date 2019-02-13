b3659e1b401b7f35d6c9f1c20c022c61b480171f
“tangz” <tangz@yiche.com>
2019-02-13 16:18:21 +0800

    添加数组分类

--- KFACommonLibDemo/KFACommonLibDemo/KFAAdditions/NSArray+KFASafe.h
+++ KFACommonLibDemo/KFACommonLibDemo/KFAAdditions/NSArray+KFASafe.h
@@ -12,6 +12,19 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface NSArray (KFASafe)
 
+- (id)kfa_objectAtIndex:(NSUInteger)index;
+
+@end
+
+@interface NSMutableArray (KFASafe)
+
+- (void)kfa_addObject:(id)anObject;
+- (void)kfa_insertObject:(id)anObject atIndex:(NSUInteger)index;
+- (void)kfa_removeObjectAtIndex:(NSUInteger)index;
+- (void)kfa_replaceObjectAtIndex:(NSUInteger)index withObject:(id)anObject;
+- (void)kfa_exchangeObjectAtIndex:(NSUInteger)idx1 withObjectAtIndex:(NSUInteger)idx2;
+- (void)kfa_removeObjectsInRange:(NSRange)range;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- KFACommonLibDemo/KFACommonLibDemo/KFAAdditions/NSArray+KFASafe.m
+++ KFACommonLibDemo/KFACommonLibDemo/KFAAdditions/NSArray+KFASafe.m
@@ -10,4 +10,53 @@
 
 @implementation NSArray (KFASafe)
 
+- (id)kfa_objectAtIndex:(NSUInteger)index {
+    if (index < self.count) {
+        return [self objectAtIndex:index];
+    }
+    return nil;
+}
+
+@end
+
+@implementation NSMutableArray (KFASafe)
+
+- (void)kfa_addObject:(id)anObject {
+    if (anObject != nil) {
+        [self addObject:anObject];
+    }
+}
+
+- (void)kfa_insertObject:(id)anObject atIndex:(NSUInteger)index {
+    if (anObject != nil) {
+        if ((index == 0 && self.count == 0) || index < self.count) {
+            [self insertObject:anObject atIndex:index];
+        }
+    }
+}
+
+- (void)kfa_removeObjectAtIndex:(NSUInteger)index {
+    if (index < self.count) {
+        [self removeObjectAtIndex:index];
+    }
+}
+
+- (void)kfa_replaceObjectAtIndex:(NSUInteger)index withObject:(id)anObject {
+    if (index < self.count) {
+        [self replaceObjectAtIndex:index withObject:anObject];
+    }
+}
+
+- (void)kfa_exchangeObjectAtIndex:(NSUInteger)idx1 withObjectAtIndex:(NSUInteger)idx2 {
+    if (idx1 < self.count && idx2 < self.count) {
+        [self exchangeObjectAtIndex:idx1 withObjectAtIndex:idx2];
+    }
+}
+
+- (void)kfa_removeObjectsInRange:(NSRange)range {
+    if (range.location+range.length <= self.count) {
+        [self removeObjectsInRange:range];
+    }
+}
+
 @end
