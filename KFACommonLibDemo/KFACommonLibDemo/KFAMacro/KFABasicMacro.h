0cf78893aebaae0fe22168f14422209fe71e1232
“tangz” <tangz@yiche.com>
2019-02-13 15:10:04 +0800

    更新宏

--- KFACommonLibDemo/KFACommonLibDemo/KFAMacro/KFABasicMacro.h
+++ KFACommonLibDemo/KFACommonLibDemo/KFAMacro/KFABasicMacro.h
@@ -9,5 +9,22 @@
 #ifndef KFABasicMacro_h
 #define KFABasicMacro_h
 
+// 屏宽高
+#define kScreenWidth ([[UIScreen mainScreen] bounds].size.width)
+#define kScreenHeight ([[UIScreen mainScreen] bounds].size.height)
+// 状态栏高度
+#define kStatusBarHeight [[UIApplication sharedApplication] statusBarFrame].size.height
+
+// 判断NSString, NSArrsy, NSDictionary, NSSet, NSNumber
+#define IsString(__string) ([(__string) isKindOfClass:[NSString class]])
+#define IsStringWithAnyText(__string) (IsString(__string) && ([((NSString *)__string) length] > 0))
+
+#define IsArray(__array) ([(__array) isKindOfClass:[NSArray class]])
+#define IsArrayWithAnyItem(__array) (IsArray(__array) && ([((NSArray *)__array) count] > 0))
+
+#define IsDictionary(__dict) ([(__dict) isKindOfClass:[NSDictionary class]])
+#define IsDictionaryWithAnyKeyValue(__dict) (IsDictionary(__dict) && ([[((NSDictionary *)__dict) allKeys] count] > 0))
+
+#define IsNumber(__number) ([(__number) isKindOfClass:[NSNumber class]])
 
 #endif /* KFABasicMacro_h */
